cb8a4f4d56f52eb48cae65f3f8f912ddfb4a0f8d
Mohammad Ainun Najib <ainun.najib2403@gmail.com>
2019-05-28 12:29:03 +0700

    dara

---
+++ android/KlinikHusada/app/src/main/java/com/kesehatan/klinikhusada/ActivityLogin.java
@@ -0,0 +1,23 @@
+package com.kesehatan.klinikhusada;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+    }
+
+    public void daftar(View view) {
+    }
+
+    public void lupapassword(View view) {
+    }
+}
