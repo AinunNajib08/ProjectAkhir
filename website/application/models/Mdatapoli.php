aa8c6441133f3b66aa550a57d57c03cb6aa87277
waragafila <ajicheater@gmail.com>
2019-06-17 14:17:35 +0700

    new poli

---
+++ website/application/models/Mdatapoli.php
@@ -0,0 +1,57 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Mdatapoli extends CI_Model
+{
+    private $_table = "poli";
+
+    public $id_poli;
+    public $poli;
+
+    public function rules()
+    {
+        return [
+            ['field' => 'id_poli',
+            'label' => 'id_poli',
+            'rules' => 'required'],
+
+            ['field' => 'poli',
+            'label' => 'poli',
+            'rules' => 'required']
+        ];
+    }
+
+    public function getAll()
+    {
+        return $this->db->get($this->_table)->result();
+    }
+    
+    public function getById($id)
+    {
+        return $this->db->get_where($this->_table, ["product_id" => $id])->row();
+    }
+
+    public function save()
+    {
+        $post = $this->input->post();
+        $this->product_id = uniqid();
+        $this->name = $post["name"];
+        $this->price = $post["price"];
+        $this->description = $post["description"];
+        $this->db->insert($this->_table, $this);
+    }
+
+    public function update()
+    {
+        $post = $this->input->post();
+        $this->product_id = $post["id"];
+        $this->name = $post["name"];
+        $this->price = $post["price"];
+        $this->description = $post["description"];
+        $this->db->update($this->_table, $this, array('product_id' => $post['id']));
+    }
+
+    public function delete($id)
+    {
+        return $this->db->delete($this->_table, array("product_id" => $id));
+    }
+}
