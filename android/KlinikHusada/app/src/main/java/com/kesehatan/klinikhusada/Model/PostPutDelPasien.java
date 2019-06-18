ee226012bbbb369dae511a122937937e32a8e194
bawik ardiyan <ardiyanramadhan4@gmail.com>
2019-06-18 22:06:44 +0700

    roid

---
+++ android/KlinikHusada/app/src/main/java/com/kesehatan/klinikhusada/Model/PostPutDelPasien.java
@@ -0,0 +1,41 @@
+package com.kesehatan.klinikhusada.Model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.gson.annotations.SerializedName;
+
+public class PostPutDelPasien extends AppCompatActivity {
+    public class PostPutDelKontak {
+        @SerializedName("no_rm")
+        String status;
+        @SerializedName("nama_pasien")
+        Pasien mPasien;
+        @SerializedName("usia")
+        String message;
+        public String getStatus() {
+            return status;
+        }
+        public void setStatus(String status) {
+            this.status = status;
+        }
+        public String getMessage() {
+            return message;
+        }
+        public void setMessage(String message) {
+            this.message = message;
+        }
+        public Pasien getKontak() {
+            return mPasien;
+        }
+        public void setKontak(Pasien Pasien) {
+            mPasien = Pasien;
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_put_del_pasien);
+    }
+}
