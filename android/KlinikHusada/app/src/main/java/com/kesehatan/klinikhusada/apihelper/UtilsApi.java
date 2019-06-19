457c69983cdaf04628398adb6bbd7e7a9a6d99d7
Mohammad Ainun Najib <ainun.najib2403@gmail.com>
2019-06-19 23:39:26 +0700

    added

--- android/KlinikHusada/app/src/main/java/com/kesehatan/klinikhusada/apihelper/UtilsApi.java
+++ android/KlinikHusada/app/src/main/java/com/kesehatan/klinikhusada/apihelper/UtilsApi.java
@@ -2,7 +2,7 @@ package com.kesehatan.klinikhusada.apihelper;
 
 public class UtilsApi {
     // 10.0.2.2 ini adalah localhost.
-    public static final String BASE_URL_API = "http://192.168.43.2/project/website/documentation/data/api/v1/";
+    public static final String BASE_URL_API = "http://192.168.43.79/project/website/documentation/data/api/v1/";
 
     // Mendeklarasikan Interface BaseApiService
     public static BaseApiService getAPIService(){
