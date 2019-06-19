078930c1a6f4b466c8d0e8d62c3636a06f3e5459
bawik ardiyan <ardiyanramadhan4@gmail.com>
2019-06-19 23:41:21 +0700

    update andro

---
+++ android/KlinikHusada/app/src/main/java/com/kesehatan/klinikhusada/PasienActivity.java
@@ -0,0 +1,75 @@
+package com.kesehatan.klinikhusada;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.kesehatan.klinikhusada.PasienAdapter;
+import com.kesehatan.klinikhusada.Model.GetPasien;
+import com.kesehatan.klinikhusada.Model.Pasien;
+import com.kesehatan.klinikhusada.apihelper.BaseApiService;
+import com.kesehatan.klinikhusada.Rest.ApiClient;
+import com.kesehatan.klinikhusada.apihelper.UtilsApi;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+public class PasienActivity extends AppCompatActivity {
+    Button btIns;
+    ApiClient mApiInterface;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    public static MainActivity ma;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pasien);
+
+
+    btIns = (Button) findViewById(R.id.btIns);
+        btIns.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(PasienActivity.this, InsertActivity.class));
+        }
+    });
+    mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+    mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+    mApiInterface = ApiClient.getClient().create(BaseApiService.class);
+    ma=this;
+    refresh();
+}
+
+    public void refresh() {
+        Call<GetPasien> pasienCall = mApiInterface.GetPasien();
+        pasienCall.enqueue(new Callback<GetPasien>() {
+            @Override
+            public void onResponse(Call<GetPasien> call, Response<GetPasien>
+                    response) {
+                List<Pasien> KontakList = response.body().getListDataPasien();
+                Log.d("Retrofit Get", "Jumlah data Kontak: " +
+                        String.valueOf(KontakList.size()));
+                mAdapter = new PasienAdapter(KontakList);
+                mRecyclerView.setAdapter(mAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<GetPasien> call, Throwable t) {
+                Log.e("Retrofit Get", t.toString());
+            }
+        });
+    }
+}
---
+++ android/KlinikHusada/app/src/main/java/com/kesehatan/klinikhusada/PasienAdapter.java
@@ -0,0 +1,62 @@
+package com.kesehatan.klinikhusada;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.kesehatan.klinikhusada.Model.Pasien;
+import java.util.List;
+
+
+public class PasienAdapter extends RecyclerView.Adapter<PasienAdapter.MyViewHolder>{
+    List<Pasien> mPasienList;
+
+    public PasienAdapter(List <Pasien> KontakList) {
+        mPasienList = KontakList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder (ViewGroup parent,int viewType){
+        View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_pasien, parent, false);
+        MyViewHolder mViewHolder = new MyViewHolder(mView);
+        return mViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder (MyViewHolder holder,final int position){
+        holder.mTextViewId.setText("Id = " + mPasienList.get(position).getNo_rm());
+        holder.mTextViewNama.setText("Nama = " + mPasienList.get(position).getNama_pasien());
+        holder.mTextViewNomor.setText("Nomor = " + mPasienList.get(position).getUsia());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mIntent = new Intent(view.getContext(), EditActivity.class);
+                mIntent.putExtra("Id", mPasienList.get(position).getNo_rm());
+                mIntent.putExtra("Nama", mPasienList.get(position).getNama_pasien());
+                mIntent.putExtra("Nomor", mPasienList.get(position).getUsia());
+                view.getContext().startActivity(mIntent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount () {
+        return mPasienList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextViewId, mTextViewNama, mTextViewNomor;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            mTextViewId = (TextView) itemView.findViewById(R.id.no_rm);
+            mTextViewNama = (TextView) itemView.findViewById(R.id.namapasien);
+            mTextViewNomor = (TextView) itemView.findViewById(R.id.tvNomor);
+        }
+    }
+}
+}
