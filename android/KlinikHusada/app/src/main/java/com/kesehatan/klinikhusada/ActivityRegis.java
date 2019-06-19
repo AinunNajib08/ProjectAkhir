32a21f11c2a7e7a2d9c08c54ee2c6c3de2aa7c68
Mohammad Ainun Najib <ainun.najib2403@gmail.com>
2019-06-19 14:58:21 +0700

    added

--- android/KlinikHusada/app/src/main/java/com/kesehatan/klinikhusada/ActivityRegis.java
+++ android/KlinikHusada/app/src/main/java/com/kesehatan/klinikhusada/ActivityRegis.java
@@ -1,5 +1,6 @@
 package com.kesehatan.klinikhusada;
 
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -8,7 +9,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.kesehatan.klinikhusada.apihelper.BaseApiService;
@@ -19,6 +22,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -28,6 +34,8 @@ import retrofit2.Response;
 public class ActivityRegis extends AppCompatActivity {
 
     EditText no_rm, tanggal;
+    private DatePickerDialog datePickerDialog;
+    private SimpleDateFormat dateFormatter;
     Button btnlogin;
     Context mcontext;
     BaseApiService mbaseApiService;
@@ -45,6 +53,16 @@ public class ActivityRegis extends AppCompatActivity {
         mcontext = this;
         mbaseApiService = UtilsApi.getAPIService();
         initComponents();
+
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+
+        tanggal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDateDialog();
+            }
+        });
+
     }
 
     public void initComponents(){
@@ -58,6 +76,24 @@ public class ActivityRegis extends AppCompatActivity {
                 requestLogin();
             }
         });
+    }
+
+    private void showDateDialog(){
+        Calendar newCalendar = Calendar.getInstance();
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+
+                tanggal.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        datePickerDialog.show();
     }
 
     public void requestLogin(){
