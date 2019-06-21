95d083a87cc1228049c9b7d68524b724955882ca
Mohammad Ainun Najib <ainun.najib2403@gmail.com>
2019-06-21 10:00:44 +0700

    pembersihan

--- android/KlinikHusada/app/src/main/java/com/kesehatan/klinikhusada/ActivityDashboard.java
+++ android/KlinikHusada/app/src/main/java/com/kesehatan/klinikhusada/ActivityDashboard.java
@@ -17,26 +17,38 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class ActivityDashboard extends AppCompatActivity {
+
+    @BindView(R.id.tvResultNama)
+    TextView tvResultNama;
+//    @BindView(R.id.btnLogout)
+//    Button btnLogout;
+//    @BindView(R.id.btnLihatDosen)
+//    Button btnLihatDosen;
+//    @BindView(R.id.btnLihatMatkul)
+//    Button btnLihatMatkul;
+//
     SharedPrefManager sharedPrefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dash);
-
-        BottomNavigationView navView = findViewById(R.id.nav_menu);
-        navView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_cointainer,
-                new DashboardFragment()).commit();
-
+        setContentView(R.layout.activity_dashboard);
+        ButterKnife.bind(this);
         sharedPrefManager = new SharedPrefManager(this);
-        DashboardFragment fragment = new DashboardFragment();
-        Bundle datapasien = new Bundle();
-        String namasaya = sharedPrefManager.getSpUsernama();
-        datapasien.putString("daar", "Sugiono");
-        fragment.setArguments(datapasien);
 
+        String namasaya = sharedPrefManager.getSpUsernama();
+
+        tvResultNama.setText(namasaya);
+//
+//        btnLogout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                sharedPrefManager.saveSPBoolean(SharedPrefManager.SP_SUDAH_LOGIN, false);
+//                startActivity(new Intent(ActivityDashboard.this, ActivityLogin.class)
+//                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+//                finish();
+//            }
+//        });
 //
 //        btnLihatDosen.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -52,24 +64,5 @@ public class ActivityDashboard extends AppCompatActivity {
 //            }
 //        });
     }
-    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                    Fragment selectedFragment = null;
 
-                    switch (menuItem.getItemId()){
-                        case R.id.navigation_dashboard:
-                            selectedFragment = new DashboardFragment();
-                            break;
-                        case R.id.navigation_profile:
-                            selectedFragment = new ProfileFragment();
-                            break;
-                    }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_cointainer,
-                            selectedFragment).commit();
-
-                    return true;
-                }
-            };
 }
